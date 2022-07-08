9ca783155e403124a212ca64d6eb6beb46594662
unknown <b.extra9@gmail.com>
2022-07-07 20:06:14 -0400

    fetch v1

--- public/index.js
+++ public/index.js
@@ -1,22 +1,26 @@
-function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
+async function main() {
+  const countContainer = document.querySelector("#count-container");
+  const incrementButton = document.querySelector("#increment-button");
+  const decrementButton = document.querySelector("#decrement-button");
 
-    let countValue = 0;
+  const response = await fetch("http://localhost:9001/counter");
+  const data = await response.json();
+  console.log("result!!", data);
 
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
-    }
+  let countValue = data.value;
 
-    function decrement(){
-        countValue--;
-        countContainer.textContent = countValue;
-    }
-
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
+  function increment() {
+    countValue++;
     countContainer.textContent = countValue;
+  }
+
+  function decrement() {
+    countValue--;
+    countContainer.textContent = countValue;
+  }
+
+  incrementButton.addEventListener("click", increment);
+  decrementButton.addEventListener("click", decrement);
+  countContainer.textContent = countValue;
 }
-main()
+main();
